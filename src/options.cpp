7f1b54f2651b4a23e2775148bb243a983e456eb6
suyashss <suyash.shringarpure@gmail.com>
2016-05-11 01:06:33 +0530

    Using getopt library to parse options now

--- src/options.cpp
+++ src/options.cpp
@@ -1,5 +1,17 @@
+#include <getopt.h>
 #include "structhdp.h"
 #include "options.h"
+
+void print_usage(){
+	cout<<"Usage: structhdp uses the following input parameters in any order:\n";
+	cout<<"-d <datafile> \n";
+	cout<<"-n <number of individuals>\n";
+	cout<<"-m <number of loci>\n";
+	cout<<"-p <ploidy>\n";
+	cout<<"-g <other-settings-file>\n";
+	cout<<"-o <output-prefix> (optional, default='structhdp')\n";
+	cout<<"-r <random seed> (optional, default=1)\n";
+}
 
 void valuegiven(int count,int len,char* arr[]){
         if(count+1==len){
@@ -21,67 +33,69 @@ void verifyoption(int option,string message,string optswitch){
 }
 
 void readopts(int len,char* arr[]){
-        int count=1,len1;
+        int opt=0,long_index=0;
         string temp;
-        int dgiven=-1,ngiven=-1,ogiven=-1,pgiven=-1,rgiven=-1,ggiven=-1,kgiven=-1,mgiven=-1;
+        int dgiven=-1,ngiven=-1,ogiven=-1,pgiven=-1,rgiven=-1,ggiven=-1,mgiven=-1;
 	SEED=1;
 	OUTPUT_PREFIX="structhdp";
-        while(count!=len){
-                temp=string(arr[count]);
-                len1=temp.size();
-                if(len1!=2){
-                        cout<<"Invalid switch "<<arr[count]<<endl;
-                        exit(1);
-                }
-                if(arr[count][1]!='h')
-                        valuegiven(count,len,arr);
-                switch (arr[count][1]) {
+	static struct option long_options[]=
+		{
+		{"data_input",required_argument,0,'d'},
+		{"output_prefix",required_argument,0,'o'},
+		{"numinds",required_argument,0,'n'},
+		{"numloci",required_argument,0,'m'},
+		{"ploidy",required_argument,0,'p'},
+		{"help",no_argument,0,'h'},
+		{"random_seed",required_argument,0,'r'},
+		{"settings_file",required_argument,0,'g'},
+		{0,0,0,0}
+		};
+	while ((opt = getopt_long(len, arr,"d:o:n:m:p:hr:g:", 
+                   long_options, &long_index )) != -1) {
+        	switch (opt) {
                         case 'h':
                                 //help option
-                                cout<<"Usage: structhdp uses the following input parameters in any order:\n";
-                                cout<<"-d <datafile> \n";
-                                cout<<"-n <number of individuals>\n";
-                                cout<<"-m <number of loci>\n";
-                                cout<<"-p <ploidy>\n";
-                                cout<<"-g <other-settings-file>\n";
-                                cout<<"-o <output-prefix> (optional, default='structhdp')\n";
-				cout<<"-r <random seed> (optional, default=1)\n";
+				print_usage();
                                 exit(0);
                         case 'd':
                                 // Data file
-                                DATAFILE=string(arr[count+1]); dgiven=1;
+                                DATAFILE=string(optarg); dgiven=1;
                                 break;
                         case 'o':
                                 //Output prefix
-                                OUTPUT_PREFIX=string(arr[count+1]); ogiven=1;
+                                OUTPUT_PREFIX=string(optarg); ogiven=1;
                                 break;
                         case 'n':
                                 // No. of individuals
-                                NUMINDS=atoi(arr[count+1]); ngiven=1;
+                                NUMINDS=atoi(optarg); ngiven=1;
                                 break;
                         case 'm':
                                 // No. of loci
-                                NUMLOCI=atoi(arr[count+1]); mgiven=1;
+                                NUMLOCI=atoi(optarg); mgiven=1;
                                 break;
                         case 'r':
                                 // Random seed
-                                SEED=atoi(arr[count+1]); rgiven=1;
+                                SEED=atoi(optarg); rgiven=1;
                                 break;
                         case 'p':
                                 // Ploidy
-                                PLOIDY=atoi(arr[count+1]); pgiven=1;
+                                PLOIDY=atoi(optarg); pgiven=1;
                                 break;
                                 //break;
                         case 'g':
                                 // Global params
-                                OTHERPARAMS=string(arr[count+1]); ggiven=1;
+                                OTHERPARAMS=string(optarg); ggiven=1;
                                 break;
+			case '?':
                         default:
-                                cout<<"Unknown switch "<<arr[count]<<endl;
+                                cout<<"Invalid input"<<endl;
                                 exit(1);
-                }
-                count+=2;
-        }
+        	}
+    	}
+	if (optind < len) {
+		cout<<"Non-option elements in input\n";
+		exit(EXIT_FAILURE);
+	}
         verifyoption(dgiven,"Datafile","-d");
         verifyoption(ngiven,"Number of individuals","-n");
         verifyoption(mgiven,"Number of loci","-m");
@@ -91,6 +105,5 @@ void readopts(int len,char* arr[]){
                 cout<<"No seed provided, using default value: "<<SEED<<endl;
         if(ogiven==-1)
                 cout<<"No output prefix provided, using default value: "<<OUTPUT_PREFIX<<endl;
+
 }
-
-
--- src/options.h
+++ src/options.h
@@ -2,6 +2,7 @@
 
 using namespace std;
 
+void print_usage();
 void valuegiven(int count,int len,char* arr[]);
 void verifyoption(int option,string message,string optswitch);
 void readopts(int len,char* arr[]);
